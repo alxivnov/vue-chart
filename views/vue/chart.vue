0504c1c58b8d831fc928c729b8f26176dda7fab6
YuryBasalai <yurybasalai@gmail.com>
2021-07-23 21:45:55 +0300

    connecting the data visualization library vis.js

--- views/vue/chart.vue
+++ views/vue/chart.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div id="chart"></div>
+		<div id="mynetwork"></div>
   </div>
 </template>
 
@@ -34,30 +35,67 @@ export default {
 			.catch((error) => {
 				console.error(error);
 			});
-			
+
+		var color = "gray";
+		var len = undefined;
+
+		var nodes = [
+			{ id: 0, label: "Cloud Atlas", group: 0 },
+			{ id: 1, label: "Tom Tykwer", group: 1 },
+			{ id: 2, label: "Lana Wachowski", group: 1 },
+			{ id: 3, label: "Lilly Wachowski", group: 1 },
+		];
+		var edges = [
+			{ from: 1, to: 0, label: 'DIRECTED' },
+			{ from: 2, to: 0, label: 'DIRECTED' },
+			{ from: 3, to: 0, label: 'DIRECTED' }
+		];
+
+		// create a network
+		var container = document.getElementById("mynetwork");
+		var data = {
+			nodes: nodes,
+			edges: edges,
+		};
 		var options = {
-			chart: {
-				type: 'bar'
+			nodes: {
+				shape: "circle",
+				size: 30,
+				font: {
+					size: 10,
+					color: "#000",
+				},
+				borderWidth: 2,
 			},
-			series: [{
-				name: 'sales',
-				data: [30,40,45,50,49,60,70,91,125]
-			}],
-			xaxis: {
-				categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+			edges: {
+				width: 2,
+				font: {
+					size: 10,
+					align: "bottom"
+				},
+			},
+		};
+		var network = new vis.Network(container, data, options);
+
+		/*
+			var options = {
+				chart: {
+					type: 'bar'
+				},
+				series: [{
+					name: 'sales',
+					data: [30,40,45,50,49,60,70,91,125]
+				}],
+				xaxis: {
+					categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+				}
 			}
-		}
 
-		var chart = new ApexCharts(document.querySelector("#chart"), options);
+			var chart = new ApexCharts(document.querySelector("#chart"), options);
 
-		chart.render();
+			chart.render();
+		*/
 	}
 }
 </script>
 
-<style scoped>
-  #chart {
-		max-width: 650px;
-		margin: 35px auto;
-	}
-</style>
