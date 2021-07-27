7cdd6dacbcf477bd222314f1f672333f9613852d
YuryBasalai <yurybasalai@gmail.com>
2021-07-27 15:00:08 +0300

    data output from the neo4j database

--- views/vue/chart.vue
+++ views/vue/chart.vue
@@ -8,27 +8,66 @@
 <script>
 export default {
 	mounted() {
-		var driver = neo4j.driver('bolt://3.235.46.32:7687',
-				neo4j.auth.basic('neo4j', 'massed-extenuations-desk'),
+		var driver = neo4j.driver('bolt://54.211.109.0:7687',
+				neo4j.auth.basic('neo4j', 'ages-synthetics-vehicle'),
 				{/* encrypted: 'ENCRYPTION_OFF' */ });
 				
 		const query =
 			`
-			MERGE (p:Person {name:$name})
-			ON MATCH SET p.lastAt = timestamp()
-			ON CREATE SET p.createAt = timestamp()
-			RETURN p.name AS name
+			MATCH (n1)-[r]->(n2) 
+			WITH
+				[n1] AS n1,
+				[n2] AS n2,
+				[r] AS r
+			UNWIND (n1 + n2) AS node
+			UNWIND r AS edge
+			RETURN {  	
+				nodes: 
+          CASE
+					WHEN node.name IS NOT NULL
+					THEN {
+						name: node.name,
+						group: labels(node),
+						id: id(node)
+					} ELSE {
+							name: node.title,
+							group: labels(node),
+							id: id(node)
+					} END,
+					edges: {
+						id: id(edge),
+						type: type(edge),
+						from: startNode(edge),
+						to: endNode(edge)
+				}
+			} AS arrayData limit 10
 		`;
 
-		const params = { "name": "Victoryia Struk" };
-
+		// const params = { "name": "" };
+		const params = {}
 		const session = driver.session({ database: "neo4j" });
 
+		var nodes = []
+		var edges = []
 		session.run(query, params)
 			.then((result) => {
 				result.records.forEach((record) => {
-					console.log(record.get('name'));
+					nodes.push({
+						id: record.get('arrayData').nodes.id.low,
+						label: record.get('arrayData').nodes.name,
+						group: record.get('arrayData').nodes.group[0]
+					})
+					edges.push({
+						id: record.get('arrayData').edges.id.low,
+						from: record.get('arrayData').edges.from.identity.low,
+						to: record.get('arrayData').edges.to.identity.low,
+						label: record.get('arrayData').edges.type
+					})
 				});
+				
+				nodes = nodes.slice().reverse().filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i).reverse()
+				edges = edges.slice().reverse().filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i).reverse()
+				createNetwork(nodes, edges)
 				session.close();
 				driver.close();
 			})
@@ -36,65 +75,36 @@ export default {
 				console.error(error);
 			});
 
-		var color = "gray";
-		var len = undefined;
-
-		var nodes = [
-			{ id: 0, label: "Cloud Atlas", group: 0 },
-			{ id: 1, label: "Tom Tykwer", group: 1 },
-			{ id: 2, label: "Lana Wachowski", group: 1 },
-			{ id: 3, label: "Lilly Wachowski", group: 1 },
-		];
-		var edges = [
-			{ from: 1, to: 0, label: 'DIRECTED' },
-			{ from: 2, to: 0, label: 'DIRECTED' },
-			{ from: 3, to: 0, label: 'DIRECTED' }
-		];
-
-		// create a network
-		var container = document.getElementById("mynetwork");
-		var data = {
-			nodes: nodes,
-			edges: edges,
-		};
-		var options = {
-			nodes: {
-				shape: "circle",
-				size: 30,
-				font: {
-					size: 10,
-					color: "#000",
-				},
-				borderWidth: 2,
-			},
-			edges: {
-				width: 2,
-				font: {
-					size: 10,
-					align: "bottom"
-				},
-			},
-		};
-		var network = new vis.Network(container, data, options);
-
-		/*
+		function createNetwork(nodes, edges) {		
+			var container = document.getElementById("mynetwork");
+			var data = {
+				nodes: nodes,
+				edges: edges,
+			};
 			var options = {
-				chart: {
-					type: 'bar'
+				nodes: {
+					shape: "circle",
+					size: 30,
+					font: {
+						size: 10,
+						color: "#000",
+					},
+					borderWidth: 2,
 				},
-				series: [{
-					name: 'sales',
-					data: [30,40,45,50,49,60,70,91,125]
-				}],
-				xaxis: {
-					categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+				edges: {
+					width: 2,
+					font: {
+						size: 10,
+						align: "bottom"
+					},
+				},
+				physics:{
+					enabled: true
 				}
-			}
+			};
+			var network = new vis.Network(container, data, options);
+		}
 
-			var chart = new ApexCharts(document.querySelector("#chart"), options);
-
-			chart.render();
-		*/
 	}
 }
 </script>
